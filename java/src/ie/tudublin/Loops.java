29d87995d264278506779163a2794ac490efd6f8
C18742005 <c18742005@mytudublin.ie>
2020-02-12 22:08:41 +0000

    more designs added

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -63,11 +63,52 @@ public class Loops extends PApplet
 		}
 	}
 
+	public void colouredCircles()
+	{
+		float diameter = 50;
+		float radius = diameter / 2;
+		int colour = 0;
+
+		for(float x = 0 + radius; x <= 500 - radius; x += diameter)
+		{
+			noStroke();
+			fill(colour, 255, 255);
+			colour += 18;
+			ellipse(x, 250, diameter, diameter);
+		}
+	}
+
+	public void drawGrid()
+	{
+		background(0);
+		stroke(90, 255, 255);
+
+		int y1 = 50;
+		int x1 = 50;
+		int i = -5;
+		
+		for(int x = 50; x <= 460; x += 40)
+		{
+			line(x, y1, x, 500 - y1);
+			text(i, x, y1 - 20);
+			i++;
+		}
+
+		i = -5;
+
+		for(int y = 50; y <= 460; y += 40)
+		{
+			line(x1, y, 500 - x1, y);
+			text(i, x1 - 30, y);
+			i++;
+		}
+	}
+
 	public void draw()
 	{	
 		background(200);
 		stroke(0);
-		rectanglePattern();
+		drawGrid();
 		/*
 		line(10, 10, 100, 10);
 		line(10, 20, 100, 20);
