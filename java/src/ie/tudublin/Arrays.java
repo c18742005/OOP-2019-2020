d99fff8edff61eef85b0b5754d18eed720715ed0
C18742005 <c18742005@mytudublin.ie>
2020-02-17 13:52:12 +0000

    added comments for better understanding

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -8,8 +8,8 @@ public class Arrays extends PApplet
 {	
 
 	//float[] rainFall = new float[12]; 
-	float[] rainFall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
-	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+	float[] rainFall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58}; // create an array for rain fall
+	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}; // create an array for months
 
 	public void settings()
 	{
@@ -18,53 +18,67 @@ public class Arrays extends PApplet
 
 	public void setup() 
 	{
+		// loop to print the amount of rainfall for each month
 		for(int i = 0 ; i < rainFall.length; i ++)
 		{
 			println(months[i] + "\t" + rainFall[i]);
 		}
 
+		// for loop to print the values in the rainfall array
 		for(float f:rainFall)
 		{
 			println(f);
 		}
 
+		// for loop to print the values in the months array
 		for(String s:months)
 		{
 			println(s);
 		}
-
-		int minIndex = 0;
+ 
+		int minIndex = 0; // min value is currently 0
+		// loop to find the month with the lowest rainfall
 		for(int i = 1 ; i < rainFall.length ; i ++)
 		{
+			// if the rainfall in the current array variable is less than the lowest rainfall variable then make the current array value the new minIndex
 			if (rainFall[i] < rainFall[minIndex])
 			{
 				minIndex = i;
 			}
 		}
-		println(months[minIndex] + " had the minimum rainfall of " + rainFall[minIndex]);
+
+		// print month with lowest rainfall
+		println(months[minIndex] + " had the minimum rainfall of " + rainFall[minIndex]); 
 		
-		int maxIndex = 0;
+		int maxIndex = 0;  // max index is currently 0
+
+		// loop to find the month with the highest rainfall 
 		for(int i = 1 ; i < rainFall.length ; i ++)
 		{
+			// if the rainfall in the current array variable is more than the highest rainfall variable then make the current array value the new maxIndex
 			if (rainFall[i] > rainFall[maxIndex])
 			{
 				maxIndex = i;
 			}
 		}
+		// print month with the max rainfall
 		println(months[maxIndex] + " had the maximum rainfall of " + rainFall[maxIndex]);
 	}
 
+	// method to draw the barchart of month and rainfall
 	void drawBarChart()
 	{
-		float w = width / (float) rainFall.length;
-		float cGap = 255 / (float) rainFall.length;
-		noStroke();
+		float w = width / (float) rainFall.length; // get width of each bar
+		float cGap = 255 / (float) rainFall.length; // get the difference of colour between each bar
+		noStroke(); // make bars have no outline
 		colorMode(HSB);
+
+		// loop to draw each bar of the bar chart
 		for(int i = 0 ; i < rainFall.length ; i ++)
 		{
 			float x = i * w;
-			fill(i * cGap, 255, 255);
-			rect(x, height, w, -rainFall[i]);
+			fill(i * cGap, 255, 255); // select the colour for each bar
+			rect(x, height, w, -rainFall[i]); // draw a square which is a bar
 		}
 	}
 
@@ -87,7 +101,7 @@ public class Arrays extends PApplet
 		}
 	}	
 
-
+	// method called 60 times per sec 
 	public void draw()
 	{	
 		background(0);		
--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -16,29 +16,33 @@ public class Loops extends PApplet
 
 	public void loops1()
 	{
-		stroke(255);
-		int numLines = (int) (30 * (mouseX / (float) width)) ;
-		float gap = width / (float) numLines;
+		stroke(255); // make lines white
+		int numLines = (int) (30 * (mouseX / (float) width)) ; // number of lines is equal to 30 times the mouse X position divided by widht of the screen
+		float gap = width / (float) numLines; // distancce between lines is width of screen divided by the number of lines
+
+		// loop to draw the lines
 		for(int i = 0 ; i <= numLines ; i ++)
 		{
 			float x = i * gap;
 			line(x, 0, width - x, height);
 			line(0, x, width, height -x);
-		}
+		} // end for loop
 	}
 
 	float offset = 0;
 
 	public void loops2()
 	{
-		int numCircles  = (int) (20 * (mouseX / (float) width)) ;
+		int numCircles  = (int) (20 * (mouseX / (float) width)) ; // make num of circles equal to 20 times the mouse X position divided by the width of the screen
 		
-		float w = width / (float) numCircles;
-		float radius = w / 2.0f;
+		float w = width / (float) numCircles; //width of each circle is width of screen divided by num of circles
+		float radius = w / 2.0f; // radius of circle is half widht of circle
+
 		colorMode(HSB);
-		float cGap = 255 / (float) (numCircles * numCircles);
+		float cGap = 255 / (float) (numCircles * numCircles); 
 		noStroke();
 
+		// loop to draw circles and change colours
 		for(int j = 0 ; j < numCircles ; j ++)
 		{		
 			for(int i = 0 ; i < numCircles ; i ++)
@@ -55,20 +59,21 @@ public class Loops extends PApplet
 
 	public void loops3()
 	{
-		float gap = width * 0.1f;
+		float gap = width * 0.1f; 
 		float halfGap = gap / 2.0f;
 		colorMode(RGB);
-		stroke(0, 255, 0);
-		textAlign(CENTER, CENTER);
+		stroke(0, 255, 0); // make lines green
+		textAlign(CENTER, CENTER); // align text
+
+		// loop to draw grid
 		for(int i = -5 ; i <=5 ; i ++)
 		{
-			float x = map(i, -5, 5, gap, width -gap);				
-			line(x, gap, x, height - gap);
-			line(gap, x, width - gap, x);
-			fill(255);
-			text(i, x, halfGap);
-			text(i, halfGap, x);
-			
+			float x = map(i, -5, 5, gap, width -gap); // remaps a number from one range to another			
+			line(x, gap, x, height - gap); // draw vertical line
+			line(gap, x, width - gap, x); // draw horizontal line
+			fill(255); // make text white
+			text(i, x, halfGap); // write text hoizontally across the screen
+			text(i, halfGap, x); // write text vertically down the screen
 		}
 	}
 
